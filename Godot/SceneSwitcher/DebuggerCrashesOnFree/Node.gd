84042ad03c12dc9728e7eb5c95b7231df759a908
hilfazer <az13337@gmail.com>
2018-08-08 18:38:16 +0200

    added dictionary

--- Godot/SceneSwitcher/DebuggerCrashesOnFree/Node.gd
+++ Godot/SceneSwitcher/DebuggerCrashesOnFree/Node.gd
@@ -20,22 +20,22 @@ func _ready():
 #	pass	# place breakpoint here; no crash
 
 
-#func _enter_tree():
-#	var arr = [ Node.new() ]
-#
-#	arr[0].free()
-#	pass	# place breakpoint here; crash
+func _enter_tree():
+	var dic = { 1 : Node.new() }
+
+	dic[1].free()
+	pass	# place breakpoint here, crash
 
 
 #func _enter_tree():
 #	arrSquareBracketFree()
 
 
-func _enter_tree():
-	var n = Node.new()
-
-	n.free()
-	pass	# place breakpoint here; no crash
+#func _enter_tree():
+#	var n = Node.new()
+#
+#	n.free()
+#	pass	# place breakpoint here; no crash
 
 
 #func _input(event):
@@ -46,7 +46,7 @@ func _enter_tree():
 func _input(event):
 	if event.is_action_pressed("ui_select"):
 		var arr = [ Node.new() ]
-	
+
 		arr[0].free()
 		pass	# place breakpoint here, crash
 
